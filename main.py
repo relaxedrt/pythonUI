3fe28079d98da8a3bdd16d45419f845f20e936e6
relaxedrt <rodrigotejeria01@gmail.com>
2023-12-21 15:00:36 +0100

    crear ventana

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-from customtkinter import CTk, CTkFrame
+from customtkinter import CTk, CTkFrame, CTkEntry, CTkLabel,CTkButton,CTkCheckBox
+from tkinter import PhotoImage
+
+root = CTk()
+
+root.geometry("500x600+350+20")
+root.minsize(400,500)
+root.config(bg= "#22272e")
+root.title("Data Saver: INGEMOTIONS")
+
+root.mainloop()
