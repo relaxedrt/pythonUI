9b37137433a3294c4697eda9ad18f39e0dad3c98
relaxedrt <rodrigotejeria01@gmail.com>
2024-01-22 11:03:13 +0100

    slider

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import customtkinter
 from tkinter import PhotoImage
 
+customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
 root = customtkinter.CTk()
+
+logo = PhotoImage(file="inge.png")
 
 root.geometry("500x600+350+20")
 root.minsize(400,500)
-root.config(bg= "#22272e",)
 root.title("Data Saver: INGEMOTIONS")
 root.iconbitmap("inge.ico")
 
-def button_function():
-    print("button pressed")
+def sliderprint():
+    print(slider._output_value)
+
+#Crear una barra de valor
+slider = customtkinter.CTkSlider(root, from_=0, to=100)
+slider.place(relx=0.5, rely=0.25, anchor=customtkinter.CENTER)
 
 # Use CTkButton instead of tkinter Button
-button = customtkinter.CTkButton(master=root, text="Inicio de turno", command=button_function)
-button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+button = customtkinter.CTkButton(root, text="Value", command=sliderprint)
+button.place(relx=0.5, rely=0.75, anchor=customtkinter.CENTER)
+
+
 
 root.mainloop()
